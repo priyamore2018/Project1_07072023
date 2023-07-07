8a3ed4fb83c0ed2e42d522319df383972684c26c
priyamore2018 <piya.more2018@example.com>
2023-07-07 11:19:39 +0200

    Adding Test_Case1.py and Test_Case2.py

---
+++ Test_Case1.py
@@ -0,0 +1,4 @@
+def Test_case_1():
+    print("I am Test_Case1.py file. welcome to Test case 1!!!")
+
+Test_case_1()
---
+++ Test_Case2.py
@@ -0,0 +1,4 @@
+def Test_case_2():
+    print("I am Test_Case2.py file. welcome to Test case 1!!!")
+
+Test_case_2()
